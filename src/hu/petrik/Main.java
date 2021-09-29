d515fa5a4ca33977e4b8357a7eb61038a33b5119
pivarcsi-gergely <gergely.pivarcsi@gmail.com>
2021-09-29 15:04:57 +0200

    bejegyzesek.txt fájl csatolása a projekthez, bejegyzesekLista felvétele kettő bejegyzéssel

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,8 +1,17 @@
 package hu.petrik;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    static List<Bejegyzes> bejegyzeekLista = new ArrayList<>();
 
+    public static void main(String[] args) {
+        Bejegyzes b1 = new Bejegyzes("Ábel", "Nem vizsgázok ősszel!");
+        Bejegyzes b2 = new Bejegyzes("Ádám", "SZeretem az őszt!");
+
+        bejegyzeekLista.add(b1);
+        bejegyzeekLista.add(b2);
     }
 }
