beb634cb2d0273474efe9a031076994439f84c23
pivarcsi-gergely <gergely.pivarcsi@gmail.com>
2021-09-29 14:59:37 +0200

    Bejegyzes osztály létrehozása, adattagok felvétele, metódusok, eljárások felvétele, toString függvény felülírása

---
+++ src/hu/petrik/Bejegyzes.java
@@ -0,0 +1,56 @@
+package hu.petrik;
+
+import com.sun.jdi.LocalVariable;
+
+import java.time.LocalDateTime;
+
+public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok = 0;
+    private LocalDateTime letrejott = LocalDateTime.now();
+    private LocalDateTime szerkesztve = LocalDateTime.now();
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+    }
+
+    public String getSZerzo() {
+        return this.szerzo;
+    }
+
+    public String getTartalom() {
+        return this.tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+        this.szerkesztve = LocalDateTime.now();
+    }
+
+    public int getLikeok() {
+        return this.likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return this.letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return this.szerkesztve;
+    }
+
+    public void like() {
+        this.likeok++;
+    }
+
+    @Override
+    public String toString() {
+        return this.szerzo + " - " + this.likeok + " - " + this.letrejott + "\n" +
+                "Szerkesztve: " + this.szerkesztve +"\n" +
+                this.tartalom;
+    }
+}
+
+
